dea2fce9b777384495f749e6193265d661075582
smajaas <smajaas@gmail.com>
2023-05-16 23:46:26 +0530

    custom instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,11 +1,18 @@
 import { useEffect } from 'react';
-
+import axios from 'axios';
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const resp = await axios(productsUrl);
+      const resp2 = await axios(randomUserUrl);
+      console.log(resp2);
+      console.log(resp);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,15 @@
 import { useEffect } from 'react';
+import axios from 'axios';
+import authFetch from '../axios/custom';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const resp1 = await authFetch('/react-store-products');
+      const resp2 = await axios(randomUserUrl);
+    } catch (error) {}
   };
 
   useEffect(() => {
