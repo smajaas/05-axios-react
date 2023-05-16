c050fb8bf12110dd3831291a8a25fd01de07d1e7
smajaas <smajaas@gmail.com>
2023-05-16 22:45:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Title from './components/Title';
+import Setup from './examples/1-first-request';
+function App() {
+  return (
+    <main>
+      <Title />
+    </main>
+  );
+}
+
+export default App;
